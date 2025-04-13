2cfd2f493111bcc09089861b40e77b15330a4026
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 23:10:32 -0400

    switched to sourcing user-setup file

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -11,7 +11,7 @@ sudo usermod -aG nix-users "$USER"
 sudo nala install curl ntp -y
 
 ## Setup for User Configuration
-echo "sh <(curl https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"
+echo "source <(curl https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"
 
 ## Reboot
 sudo reboot

e3829753db28467f99a4b668ec28ef1dacd13db2
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 21:11:38 -0400

    temporarily disabled git repository in home-manager config for install

--- debian-user-setup.sh
+++ debian-user-setup.sh
@@ -10,7 +10,11 @@ mkdir -p "$HOME/.config"
 nix-shell -p git --run\
   "git clone https://github.com/TressaDanvers/home-manager.git \"$HOME/.config/home-manager\""
 
+mv "$HOME/.config/home-manager/.git" "$HOME/.config/home-manager/.git.bak"
+
 nix-shell -p home-manager --run\
   "home-manager switch --flake \"$HOME/.config/home-manager/#default\" --extra-experimental-features \"nix-command flakes\" -b bak"
+
+mv "$HOME/.config/home-manager/.git.bak" "$HOME/.config/home-manager/.git"
 
 home-manager switch

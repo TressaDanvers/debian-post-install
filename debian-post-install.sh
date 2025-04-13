2cf63197a45f75a2ea32a600567074b311d89228
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 21:28:46 -0400

    added automatic restart and user-configuration

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -6,3 +6,12 @@ sudo apt install nala -y
 ## Nix Package Manager
 sudo nala install nix -y
 sudo usermod -aG nix-users "$USER"
+
+## System Utilities
+sudo nala install curl -y
+
+## Setup for User Configuration
+echo "sh <(https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"
+
+## Reboot
+sudo reboot

25a289492b15c5d156c416b3c329dbd94c36d458
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 22:34:46 -0400

    made initial install impure

--- debian-user-setup.sh
+++ debian-user-setup.sh
@@ -13,7 +13,7 @@ nix-shell -p git --run\
 mv "$HOME/.config/home-manager/.git" "$HOME/.config/home-manager/.git.bak"
 
 nix-shell -p home-manager --run\
-  "home-manager switch --flake \"$HOME/.config/home-manager/#debian-minimum\" --extra-experimental-features \"nix-command flakes\" -b bak"
+  "home-manager switch --impure --flake \"$HOME/.config/home-manager/#debian-minimum\" --extra-experimental-features \"nix-command flakes\" -b bak"
 
 mv "$HOME/.config/home-manager/.git.bak" "$HOME/.config/home-manager/.git"
 

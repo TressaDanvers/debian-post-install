f0970563c69df6374f9ffcfd04bf0925d74bbc8b
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 22:55:53 -0400

    added ntp to system utilities

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -8,7 +8,7 @@ sudo nala install nix -y
 sudo usermod -aG nix-users "$USER"
 
 ## System Utilities
-sudo nala install curl -y
+sudo nala install curl ntp -y
 
 ## Setup for User Configuration
 echo "sh <(curl https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"

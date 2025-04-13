faa7c99d4174d3a27851914840fe8543d4098f1f
Tressa Danvers <TDanvers@protonmail.ch>
2025-04-12 22:02:46 -0400

    error correction

--- debian-post-install.sh
+++ debian-post-install.sh
@@ -11,7 +11,7 @@ sudo usermod -aG nix-users "$USER"
 sudo nala install curl -y
 
 ## Setup for User Configuration
-echo "sh <(https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"
+echo "sh <(curl https://raw.githubusercontent.com/TressaDanvers/debian-post-install/refs/heads/master/debian-user-setup.sh)" >> "$HOME/.profile"
 
 ## Reboot
 sudo reboot
--- debian-user-setup.sh
+++ debian-user-setup.sh
@@ -13,10 +13,8 @@ nix-shell -p git --run\
 mv "$HOME/.config/home-manager/.git" "$HOME/.config/home-manager/.git.bak"
 
 nix-shell -p home-manager --run\
-  "home-manager switch --flake \"$HOME/.config/home-manager/#$USER\" --extra-experimental-features \"nix-command flakes\" -b bak"
+  "home-manager switch --flake \"$HOME/.config/home-manager/#debian-minimum\" --extra-experimental-features \"nix-command flakes\" -b bak"
 
 mv "$HOME/.config/home-manager/.git.bak" "$HOME/.config/home-manager/.git"
 
-nix-shell -p home-manager --run "home-manager switch"
-
-exec bash
+home-manager switch
